e93d7fa0663940d7e126f8c5d2ecc5d60fef0903
jackelinecargav <jackelinel.cardenasgavino@gmail.com>
2021-07-15 14:50:59 -0500

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,100 +1,92 @@
 <template>
-  <div>
-    <div class="hello">
-      <el-form>
-        <el-input
-          placeholder="Ingrese su usuario"
-          v-model="usuario"
-          maxlength="8"
-        >
-          <i class="el-icon-user el-input__icon" slot="suffix"></i> </el-input
-        ><br /><br />
-
-        <el-input
-          placeholder="Ingrese su contraseña"
-          maxlength="8"
-          v-model="contrasena"
-          show-password
-        >
-        </el-input>
-      </el-form>
-
-      <div class="desk">
-        <div><p style="font-size: 14px">Formaato DDMMAAAA</p></div>
-        <div>
-          <p style="font-size: 14px" class="textColor">
-            Ejemplo: Si naciste el 01 de abril de 1965, debe ingresar: 01041965
-          </p>
+  <div class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static ">
+      <div class="app-content content ">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <div class="auth-wrapper auth-v2">
+                    <div class="auth-inner row m-0">
+                        <!-- Brand logo--><a class="brand-logo" href="javascript:void(0);">
+                           
+                            <h2 class="brand-text text-primary ml-1">ESTEC</h2>
+                        </a>
+                        <!-- /Brand logo-->
+                        <!-- Left Text-->
+                        <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
+                            <div class="w-100 d-lg-flex align-items-center justify-content-center px-5"><img class="img-fluid" src="app-assets/images/pages/login-v2.svg" alt="Login V2" /></div>
+                        </div>
+                        <!-- /Left Text-->
+                        <!-- Login-->
+                        <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
+                            <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
+                                <h2 class="card-title font-weight-bold mb-1">ESTEC</h2>
+                                <p class="card-text mb-2">Portal pago a provedores</p>
+                                <form class="auth-login-form mt-2" action="maqueta/estec-DE-Bandeja.html" method="">
+                                    <div class="form-group">
+                                        <label class="form-label" for="login-email">Usuario</label>
+                                        <input class="form-control" id="login-email" type="text" name="login-email2" placeholder="" aria-describedby="login-email" autofocus="" tabindex="1" />
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="d-flex justify-content-between">
+                                            <label for="login-password">Contrase&ntilde;a</label><a href="RecuperarLogin.html"><small>Olvidaste tu contrase&ntilde;a?</small></a>
+                                        </div>
+                                        <div class="input-group input-group-merge form-password-toggle">
+                                            <input class="form-control form-control-merge" id="login-password" type="password" name="login-password" placeholder="" aria-describedby="login-password" tabindex="2" />
+                                            <div class="input-group-append"><span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span></div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox">
+                                            <input class="custom-control-input" id="remember-me" type="checkbox" tabindex="3" />
+                                            <label class="custom-control-label" for="remember-me"> Recordar Usuario</label>
+                                        </div>
+                                    </div>
+                                    <button class="btn btn-primary btn-block" tabindex="4">Ingresar</button>
+                                </form>
+								<p class="text-center mt-2"><a href="SolicitarCuenta.html"><span>&nbsp;Solicitar Cuenta</span></a></p>
+                               
+                            </div>
+                        </div>
+                        <!-- /Login-->
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <br />
-      <router-link
-        :to="{ name: 'ContainListas', params: { logeado: 'true' } }"
-        replace
-      >
-        <el-button
-          style="background-color: #026433; color: white"
-          @click="siguiente()"
-          >INGRESAR
-        </el-button></router-link
-      >
     </div>
-  </div>
+    </div>
+  
 </template>
-
+ <script src="app-assets/vendors/js/vendors.min.js"></script>
+ <script src="app-assets/vendors/js/forms/validation/jquery.validate.min.js"></script>
+ <script src="app-assets/js/core/app-menu.js"></script>
+ <script src="app-assets/js/core/app.js"></script>
+ <script src="app-assets/js/scripts/pages/page-auth-login.js"></script>
 <script>
 export default {
   data() {
     return {
-      loading: false,
-      usuario: "",
-      contrasena: "",
-      parametro: 1,
+    
     };
   },
   created() {
-    //this.parametro=
+     $(window).on('load', function() {
+            if (feather) {
+                feather.replace({
+                    width: 14,
+                    height: 14
+                });
+            }
+        })
   },
   methods: {
-    siguiente() {
-      this.$router.replace("/contain");
-    },
-    //   soloNumeros: function(event){
-    //   var key = window.event ? event.which : event.keyCode;
-    //   if(key == 8){
-    //   }else if (key < 48 || key > 57) {
-    //     event.preventDefault();
-    //   }
-    // }
+   
   },
 };
 </script>
+
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.textColor {
-  color: red;
-}
-.desk {
-  column-count: 2;
-  text-align: left;
-}
-@media screen and (max-width: 600px) {
-  .desk {
-    column-count: 1;
-    text-align: left;
-  }
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,8 @@
 <template>
   <el-row>
-    <div
-      class="question"
-      v-loading="loading"
-      element-loading-text="Loading..."
-      element-loading-spinner="el-icon-loading"
-      element-loading-background="rgba(0, 0, 0, 0.8)"
-    >
-    <div class="flex-container">
-      <div class="thank">
-        <div
-          style="
-            column-count: 2;
-            background: linear-gradient(-90deg, #026433, white);
-          "
-        >
-          <br />
-          <div>
-            <img
-              style="width: 100px; height: 100px; text-aling: lefth"
-              alt="Vue logo"
-              src="../assets/logo_smg.png"
-            /><br />
-          </div>
-          <div>
-            <br />
-            <p style="color: white; font-weight: bold">VOTO ELECTRÓNICO</p>
-            <p style="color: #76f63a; font-weight: bold">
-              Elecciones Complementarias para Delegados 2021
-            </p>
-          </div>
-        </div>
+    <div >
+   
+     
 
         <div v-if="login">
           <Terminado @nuevoVoto='limpiarVoto'/>
@@ -40,10 +12,7 @@
         </div><br>
         
       </div>
-      <div class="item"><p style="font-size: 14px; color: red">
-          En caso de inconvenientes llamar: 923536657, 923536841, 913005754
-        </p></div></div>
-    </div>
+     
   </el-row>
 </template>
 
